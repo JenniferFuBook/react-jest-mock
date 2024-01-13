154dd611a27e5c5ce555c9efb7ba9258e6e602cb
jenniferjfu2 <jennifer.fu@dominodatalab.com>
2024-01-12 21:04:39 -0800

    change file extension

---
+++ src/useDate.test.ts
@@ -0,0 +1,14 @@
+import { renderHook } from '@testing-library/react';
+import useDate from './useDate';
+
+describe('useDate', () => {
+  it('should return the default locale and associated formats', async () => {
+    const { result } = renderHook(() => useDate());
+    const date = '1/10/2024 6:10 PM';
+    expect(result.current.locale).toBe('en');
+    expect(result.current.formatDateTime(date)).toBe('01/10/2024 6:10 PM');
+    expect(result.current.formatTime(date)).toBe('6:10 PM');
+    expect(result.current.formatFullDate(date)).toBe('Jan 10, 2024');
+    expect(result.current.formatFullDateTime(date)).toBe('Jan 10, 2024 6:10 PM');
+  });
+});
